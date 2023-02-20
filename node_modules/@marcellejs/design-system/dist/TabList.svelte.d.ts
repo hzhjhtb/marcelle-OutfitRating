61c67b61063893f46b3092e7981b0e71ab21aabd
黄哲 <zhehuang@huangzhedeMacBook-Pro.local>
2023-02-20 20:37:03 +0100

    Creation of our own site with marcelle

---
+++ node_modules/@marcellejs/design-system/dist/TabList.svelte.d.ts
@@ -0,0 +1,23 @@
+/** @typedef {typeof __propDef.props}  TabListProps */
+/** @typedef {typeof __propDef.events}  TabListEvents */
+/** @typedef {typeof __propDef.slots}  TabListSlots */
+export default class TabList extends SvelteComponentTyped<{}, {
+    [evt: string]: CustomEvent<any>;
+}, {
+    default: {};
+}> {
+}
+export type TabListProps = typeof __propDef.props;
+export type TabListEvents = typeof __propDef.events;
+export type TabListSlots = typeof __propDef.slots;
+import { SvelteComponentTyped } from "svelte";
+declare const __propDef: {
+    props: {};
+    events: {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {
+        default: {};
+    };
+};
+export {};
---
+++ src/index.js
@@ -0,0 +1,62 @@
+import '@marcellejs/core/dist/marcelle.css';
+import * as marcelle from '@marcellejs/core';
+
+// Main components
+const UpperInput = marcelle.imageUpload(Image={ width: 224, height: 224 });
+UpperInput.title = 'Upload Upper body';
+const LowerInput = marcelle.imageUpload(Image={ width: 224, height: 224 });
+LowerInput.title = 'Uploead Lower body';
+const ShoesInput = marcelle.imageUpload(Image={ width: 224, height: 224 });
+ShoesInput.title = 'Uploead Shoes';
+// const featureExtractor = marcelle.mobileNet();
+// featureExtractor.title = 'Feature Extractor';
+// const trainingSet = marcelle.dataset('TrainingSet');
+// const model = 
+
+// Additional widgets and visualizations
+const texture_label = marcelle.textInput();
+texture_label.title = 'texture score';
+const color_label = marcelle.textInput();
+color_label.title = 'color score';
+const shape_label = marcelle.textInput();
+shape_label.title = 'shape score';
+
+const capture = marcelle.button('Click to record an instance');
+capture.title = 'Capture this outfit to training set';
+const trainButton = marcelle.button('Click to train the model');
+trainButton.title = 'Train';
+const predictButton = marcelle.button('Click to test');
+predictButton.title = 'Let model rate this outfit!'
+
+const UpperDisplay = marcelle.imageDisplay(UpperInput.$images);
+UpperDisplay.title = 'Upper body';
+const LowerDisplay = marcelle.imageDisplay(LowerInput.$images);
+LowerDisplay.title = 'Lower body';
+const ShoesDisplay = marcelle.imageDisplay(ShoesInput.$images);
+ShoesDisplay.title = 'Shoes';
+// const progress = marcelle.trainingProgress();
+
+
+
+
+
+
+const myDashboard = marcelle.dashboard({
+	title: 'Outfit Rating',
+	author: 'Dana Aubakirova, Dorin Doncenco, Zhe Huang, Heidi Sainte-Catherine',
+	});
+
+myDashboard
+	.page('Train outfit rating system')
+	.sidebar(UpperInput, LowerInput, ShoesInput)
+	.use([UpperDisplay, LowerDisplay, ShoesDisplay])
+	.use([texture_label, color_label, shape_label, capture])
+	.use(trainButton);
+
+myDashboard
+	.page('Test outfit rating system')
+	.sidebar(UpperInput, LowerInput, ShoesInput)
+	.use([UpperDisplay, LowerDisplay, ShoesDisplay])
+	.use(predictButton);
+  
+myDashboard.show();
